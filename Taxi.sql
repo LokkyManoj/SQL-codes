ed91844812671c205d655f058ed7bad8e1e658e5
LokkyManoj <113964739+LokkyManoj@users.noreply.github.com>
2024-04-18 17:21:17 +0530

    Add files via upload
    For taxi pojo 1

--- Taxi.sql
+++ Taxi.sql
@@ -8,9 +8,12 @@ insert into taxi values("Gopika",1000,"Mini");
 insert into taxi values("Boopathi",300,"Micro");
 insert into taxi values("saranya",2000,"Micro");
 insert into taxi values("Renuga",700,"Prime");
-update anime set Name="Sriraman" where Distance=500;
-update anime set Cab_Type="Prime" where Name=Gopika;
-alter table taxi modify Name varchar(20);
+insert into taxi values("Karishma",800,"Prime");
 
+update taxi set Name="Sriraman" where Distance=500;
+update taxi set Cab_Type="Prime" where Name="Gopika";
+alter table taxi modify Name varchar(20);
+alter table taxi add column ID int auto_increment primary key;
+delete from taxi where id =2;
 
 select*from taxi order by Name;
